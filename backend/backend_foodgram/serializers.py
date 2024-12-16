4569208f35c0517431d5b76cb50406d8babbcd62
Maksim Polushin <btnchsky.sad@mail.ru>
2024-12-17 02:58:41 +0300

    Ingredient serializer

--- backend/backend_foodgram/serializers.py
+++ backend/backend_foodgram/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Tag
+from .models import Tag, Ingredient
 
 
 class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name', 'slug',)
+
+
+class IngredientSerializer(ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measurement_unit',)
