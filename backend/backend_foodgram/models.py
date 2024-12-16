450ffdb10f9ba4a0e26bedbe2ebb188bc52270b3
Maksim Polushin <btnchsky.sad@mail.ru>
2024-12-17 02:56:17 +0300

    Ingredient model

--- backend/backend_foodgram/models.py
+++ backend/backend_foodgram/models.py
@@ -1,5 +1,13 @@
 from django.db.models import (
-    Model, CharField, SlugField
+    Model, CharField, SlugField, Choices
+)
+
+MEASUREMENT_UNIT_CHOICES = (
+    ('kg', 'кг'),
+    ('g', 'г'),
+    ('mg', 'мг'),
+    ('l', 'л'),
+    ('ml', 'мл')
 )
 
 
@@ -7,3 +15,10 @@ class Tag(Model):
     name = CharField(verbose_name='Название', max_length=16)
     slug = SlugField(verbose_name='Слаг', max_length=32)
 
+
+class Ingredient(Model):
+    name = CharField(verbose_name='Ингредиент', max_length=128)
+    measurement_unit = CharField(
+        max_length=4, choices=MEASUREMENT_UNIT_CHOICES,
+        verbose_name='Мера измерения'
+    )
