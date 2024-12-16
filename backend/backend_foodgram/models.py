e367fba8dff70a3bcabd70ef872e87f829b7a1bf
Maksim Polushin <btnchsky.sad@mail.ru>
2024-12-17 02:16:31 +0300

    small changes

--- backend/backend_foodgram/models.py
+++ backend/backend_foodgram/models.py
@@ -1,1 +1,9 @@
-from django.db import models
+from django.db.models import (
+    Model, CharField, SlugField
+)
+
+
+class Tag(Model):
+    name = CharField(verbose_name='Название', max_length=16)
+    slug = SlugField(verbose_name='Слаг', max_length=32)
+
