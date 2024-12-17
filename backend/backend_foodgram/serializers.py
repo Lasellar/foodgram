2756d09fd09cb6a31f7ee9296d7f6127ec3a93ac
Maksim Polushin <btnchsky.sad@mail.ru>
2024-12-17 21:16:54 +0300

    recipe serializer carcas

--- backend/backend_foodgram/serializers.py
+++ backend/backend_foodgram/serializers.py
@@ -1,6 +1,10 @@
+import base64
+
 from rest_framework.serializers import ModelSerializer
 
-from .models import Tag, Ingredient
+from .models import (
+    Tag, Ingredient, Recipe, RecipeIngredient, RecipeTag
+)
 
 
 class TagSerializer(ModelSerializer):
@@ -13,3 +17,17 @@ class IngredientSerializer(ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit',)
+
+
+class RecipeSerializer(ModelSerializer):
+    ingredients = IngredientSerializer(many=True)
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'tags', 'author', 'ingredients', 'is_favorited',
+            'is_in_shoppting_cart', 'name', 'image', 'text', 'cooking_time'
+        )
+        read_only_fields = ('author',)
+
