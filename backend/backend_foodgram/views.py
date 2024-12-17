5147c77ed565ea9dff6c3ba0a011b5429d83ed9a
Maksim Polushin <btnchsky.sad@mail.ru>
2024-12-17 03:03:00 +0300

    add IngredientViewSet & fix TagViewSet

--- backend/backend_foodgram/views.py
+++ backend/backend_foodgram/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from rest_framework.mixins import (
-    CreateModelMixin, ListModelMixin
+    CreateModelMixin, ListModelMixin, RetrieveModelMixin
 )
 from rest_framework.permissions import IsAdminUser
 
-from .models import Tag
-from .serializers import TagSerializer
+from .models import (
+    Tag, Ingredient,
+)
+from .serializers import (
+    TagSerializer, IngredientSerializer
+)
 
 
-class TagViewSet(CreateModelMixin, ListModelMixin):
+class TagViewSet(RetrieveModelMixin, ListModelMixin):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (IsAdminUser,)
 
 
+class IngredientViewSet(RetrieveModelMixin, ListModelMixin):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (IsAdminUser,)
