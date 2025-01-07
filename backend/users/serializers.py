983c242e6d87f9aaabfc5e3c7240e2789f8abbbe
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-07 15:49:59 +0300

    UserSubscribeRepresentSerializer & UserSubscribeSerializer(maybe not working)

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework.serializers import (
-    ModelSerializer, CharField, SerializerMethodField
+    ModelSerializer, CharField, SerializerMethodField,
+    ValidationError
 )
 
 from .models import Subscription
+from ..backend_foodgram.serializers import RecipeShortSerializer
 
 User = get_user_model()
 
@@ -18,7 +20,7 @@ class UserSignUpSerializer(UserCreateSerializer):
         )
 
 
-class UserGETSerializer(ModelSerializer):
+class UserGETSerializer(UserSerializer):
     is_subscribed = SerializerMethodField()
 
     class Meta:
@@ -34,3 +36,54 @@ class UserGETSerializer(ModelSerializer):
             user.is_authenticated
             and Subscription.objects.filter(user=user, author=obj).exists()
         )
+
+
+class UserSubscribeRepresentSerializer(UserGETSerializer):
+    recipes = SerializerMethodField()
+    recipes_count = SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'id', 'username', 'first_name', 'last_name',
+            'is_subscribed', 'recipes', 'recipes_count'
+        )
+        read_only_fields = (
+            'email', 'username', 'first_name', 'last_name',
+            'is_subscribed', 'recipes', 'recipes_count'
+        )
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        recipes_limit = None
+        recipes = obj.recipes.all()
+
+        if request:
+            recipes_limit = request.query_params.get('recipes_limit')
+        if recipes_limit:
+            recipes = obj.recipes.all()[:int(recipes_limit)]
+
+        return RecipeShortSerializer(
+            recipes, many=True, context={'request': request}
+        ).data
+
+
+class UserSubscribeSerializer(ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+    def validate(self, data):
+        request = self.context.get('request')
+        if request.user == data['author']:
+            raise ValidationError(
+                'Вы пытаетесь подписаться на себя.'
+            )
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        author = instance.author
+        return UserSubscribeRepresentSerializer(
+            author, context={'request': request}
+        ).data
