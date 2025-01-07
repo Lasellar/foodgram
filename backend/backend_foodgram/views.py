5b4ea14548a4c8dbea99de530b3eb44d501aa468
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-07 16:18:56 +0300

    users.views --> backend_foodgram.views

--- backend/backend_foodgram/views.py
+++ backend/backend_foodgram/views.py
@@ -1,26 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.http import FileResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import (
     ReadOnlyModelViewSet, GenericViewSet, ModelViewSet
 )
-from rest_framework.mixins import (
-    CreateModelMixin, ListModelMixin, RetrieveModelMixin
-)
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAdminUser
 
 from .filters import IngredientFilter
-from .models import (
-    Tag, Ingredient, Recipe, ShoppingCart, Favorite
-)
+from .models import Tag, Ingredient, Recipe, ShoppingCart, Favorite
+from ..users.models import Subscription
 from .serializers import (
     TagSerializer, IngredientSerializer, RecipeCreateSerializer,
-    IngredientGETSerializer, RecipeGETSerializer, ShoppingCartSerializer,
-    FavoriteSerializer
+    RecipeGETSerializer, ShoppingCartSerializer, FavoriteSerializer,
+    UserSubscribeSerializer, UserSubscribeRepresentSerializer
 )
+
+User = get_user_model()
 
 
 class TagViewSet(ReadOnlyModelViewSet):
@@ -110,6 +111,41 @@ class RecipeViewSet(ModelViewSet):
                 user=request.user, recipe=recipe
             ).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserSubscriptionView(APIView):
+    http_method_names = ('post', 'delete')
+
+    def post(self, request, user_id):
+        author = get_object_or_404(User, id=user_id)
+        serializer = UserSubscribeSerializer(
+            data={'user': request.user.id, 'author': author.id},
+            context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, user_id):
+        user = request.user.id
+        if Subscription.objects.filter(user=user, author=user_id).exists():
+            Subscription.objects.get(user=user, author=user_id).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {'errors': 'Вы не подписаны на этого пользователя.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class UserSubscriptionsViewSet(ListModelMixin, GenericViewSet):
+    serializer_class = UserSubscribeRepresentSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(following__user=self.request.user)
+
 
 
 
