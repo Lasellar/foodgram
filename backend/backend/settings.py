34d119320dcf9747c242f4aed5b1f3860dd633dc
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-14 20:54:52 +0300

    STATIC_ROOT & STATIC_URL changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,8 +107,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
