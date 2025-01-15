8dc73d92247aa138ee9410e8d4e6512b8ae85f96
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-15 18:47:20 +0300

    fix related import

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from ..backend_foodgram.views import redirect_short_link_view
+from backend_foodgram.views import redirect_short_link_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
