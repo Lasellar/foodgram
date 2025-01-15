611c283da145a735b417503a02a50aefa92cddec
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-16 00:56:05 +0300

    users admin pannel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,13 +6,13 @@ from .models import User, Subscription
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'email', 'username', 'first_name', 'last_name')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('username', 'email')
+    list_display = ('id', 'email', 'username', 'first_name', 'last_name')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'is_staff')
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user', 'author')
+    list_display = ('id', 'user', 'author')
     search_fields = ('user', 'author')
     list_filter = ('user', 'author')
