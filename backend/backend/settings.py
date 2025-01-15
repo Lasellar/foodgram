d9a9bbb564116b9ef86c08c8584e8d9607ef26ed
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-15 03:12:27 +0300

    alias

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,8 +103,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'backend_static/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
